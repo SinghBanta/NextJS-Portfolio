ca755b2d6fc83d4fdc7405e62d0fc2c7282fbe1d
SinghBanta <singhbanta84@gmail.com>
2024-09-24 16:48:53 +0530

    Resume uploaded

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,17 +5,26 @@ import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { WorkHistory } from "@/components/WorkHistory";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Container>
       <span className="text-4xl">💼</span>
+
+      <div className="flex items-center gap-4">
       <Heading className="font-black">Education</Heading>
+      <button className="border-2 hover:bg-slate-700 rounded-full pl-3 pr-3 pt-1 pb-1 font-medium bg-slate-800 text-white text-sm transition-all duration-500 ease-in-out">
+        <Link href="/BantaSingh_Resume.pdf" target="_blank">Resume &darr;</Link>{" "}
+      </button>
+      </div>
+
       <Paragraph className="max-w-xl mt-4">
         I&apos;m a full-stack developer that loves{" "}
         <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
+        millions of lives.
       </Paragraph>
+
       <WorkHistory />
     </Container>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -25,7 +25,7 @@ export const Sidebar = () => {
             animate={{ x: 0 }}
             transition={{ duration: 0.2, ease: "linear" }}
             exit={{ x: -200 }}
-            className="px-6  z-[100] pt-10 pb-20 bg-neutral-100 w-48  fixed lg:relative  h-screen left-0 flex flex-col justify-between"
+            className="px-6  z-[100] pt-10 pb-20 sm:pb-10 bg-neutral-100 w-48  fixed lg:relative  h-screen left-0 flex flex-col justify-between"
           >
             <div className="flex-1 overflow-auto">
               <SidebarHeader />
